76b4bd289c4463cd77977d33b8ad6d06d0d45412
Aymen <collaymen.alnamer@gmail.com>
2023-03-15 18:14:41 -0400

    moved pass and username to gitignore

--- express-biztime/db.js
+++ express-biztime/db.js
@@ -1,13 +1,13 @@
 /** Database setup for BizTime. */
-
+import { pass } from "./dbPass";
 const { Client } = require("pg");
 
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql://alnamer:alnamer@localhost/biztime_test";
+  DB_URI = `postgresql://${pass}@localhost/biztime_test`;
 } else {
-  DB_URI = "postgresql://alnamer:alnamer@localhost/biztime";
+  DB_URI = `postgresql://${pass}@localhost/biztime`;
 }
 
 let db = new Client({
